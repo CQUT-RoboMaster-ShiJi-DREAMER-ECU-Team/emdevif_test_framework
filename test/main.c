d27db51c0a8a232cedbd28708983f08d883e4548
杜以成 <duyicheng741@outlook.com>
2025-06-04 21:32:57 +0800

    重新设计测试框架实现的方式，将每个测试检查/断言改为分组测试；将内部需要的函数由依赖链接调用函数使用改为通过函数指针转递回调函数的方式；增加指定换行符的流程

--- test/main.c
+++ test/main.c
@@ -7,9 +7,36 @@
 
 #include "rmdev_test_framework.h"
 
+#include <stdbool.h>
+
+#include "test_function_impl.h"
+
+void my_testItem(void)
+{
+    RMDEV_TEST_ITEM("Constant Test");
+    RMDEV_TEST_CHECK(true);
+    RMDEV_TEST_CHECK(false);
+
+    RMDEV_TEST_ITEM("Calculate Test");
+    RMDEV_TEST_CHECK((1 + 1) == 2);
+    RMDEV_TEST_CHECK((2 * 2) == 4);
+    RMDEV_TEST_CHECK((3 - 1) == 2);
+
+    RMDEV_TEST_CHECK((1 + 1) == 3);  // false
+
+    RMDEV_TEST_CHECK((114514 + 1919810) == 2034324);
+    RMDEV_TEST_ASSERT(-1 - 2 == -3);
+
+    RMDEV_TEST_ITEM("Assert Test");
+    RMDEV_TEST_ASSERT(true);
+    RMDEV_TEST_ASSERT(false);
+
+    RMDEV_TEST_CHECK(true);
+}
+
 int main(void)
 {
-    rmdev_test_framework_main();
+    rmdev_test_framework_main("\n", my_printf, my_delay, my_testItem);
 
     return 0;
 }
