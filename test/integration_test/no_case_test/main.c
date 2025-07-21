e44a30a967fa8ba8cc9d3d93dca4f00b0ae542c0
DuYicheng <duyicheng741@outlook.com>
2025-07-21 22:14:38 +0800

    增加 no_suit_test 与 no_case_test

---
+++ test/integration_test/no_case_test/main.c
@@ -0,0 +1,54 @@
+/**
+ * @file main.c
+ * @author 杜以成
+ * @date 2025-06-03
+ * @brief 测试框架的测试主函数
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "rmdev_test_framework-no_prefix.h"
+
+#include "test_function_impl.h"
+
+static TestFunc func;
+
+static void my_printf(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    vfprintf(func.file, format, args);
+
+    va_end(args);
+}
+
+TEST_SUIT(NoCaseTest) {}
+
+static void testEntry(void)
+{
+    RUN_SUIT(NoCaseTest);
+}
+
+static void testFinishHandler(void)
+{
+    exit(0);
+}
+
+int main(void)
+{
+    TestFunc_Init(&func, "no_case_test.out");
+
+    const rmdev_test_Callbacks cb = {.printfCallback = my_printf,
+                                     .delayCallback = delayImpl,
+                                     .testEntryCallback = testEntry,
+                                     .testFinishCallback = testFinishHandler};
+
+    rmdev_test_framework_main("\n", &cb, NULL);
+
+    TestFunc_End(&func);
+
+    return 0;
+}
